95e6bb348eb801d0324fe623603ed4babd1a5efe
calebgibbs <calebgibbs@me.com>
2016-07-20 09:55:38 +1200

    Add files via upload

---
+++ all-vege/js/animation.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('#search-control').click(function(){
+		$('#search-s').toggle({"display" : "none"}); 
+		$('#search-bar').toggle({"display" : "inline-block"}); 
+	});  
+
+	$('.clicked').click(function(){
+		$(this).toggleClass('clicked');
+	});
+
+	$('#account').click(function(){
+		$('#acc-box').slideToggle({"display" : "block"});
+	});
+
+	$('#v-account').click(function(){
+		$('#acc-box').slideToggle({"display" : "block"});
+	});   
+
+	$('.controller').click(function(){
+		$(this).next('.content').slideToggle({"display" : "block"});
+	});
+
+});
