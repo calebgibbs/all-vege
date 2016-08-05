c12cf670e645f5d55aae3d9bfa1917b35f52a1e5
Caleb Gibbs <calebgibbs@me.com>
2016-08-06 10:53:53 +1200

    Updating the database

--- all_vege.sql
+++ all_vege.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 31, 2016 at 11:14 AM
+-- Generation Time: Aug 06, 2016 at 12:52 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -31,16 +31,19 @@ CREATE TABLE `users` (
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `profile_picture` varchar(50) CHARACTER SET utf8mb4 NOT NULL,
   `password` varchar(60) NOT NULL,
-  `privilege` enum('user','admin') NOT NULL
+  `privilege` enum('user','moderator','admin') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `privilege`) VALUES
-(1, 'Caleb', 'Gibbs', 'calebgibbs@me.com', '$2y$10$o8lO0IKQ9vQ72bygowET2euzDHqN7enacBnhaC4XW6J2WiWFuhmia', 'admin');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `profile_picture`, `password`, `privilege`) VALUES
+(1, 'Caleb', 'Gibbs', 'calebgibbs@me.com', '', '$2y$10$uN1fPRNEof5SdJ2rBzikouhMHa9bV7/dUyeyYMVFEq/whnQJc7fci', 'admin'),
+(2, 'User', 'Account', 'user@test.com', '', '$2y$10$/eMs0H3mvq6hXTUAzz/1Wu9AfRRJJZwbIKAq8LTPZE0U0QiLTpkoS', 'user'),
+(3, 'Moderator ', 'Account', 'mod@test.com', '', '$2y$10$lwO.Gm1152cLfCBVeFFUQ.fB71g7Fx970HMWJwgUJ.OwIjI9KdEe6', 'moderator');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
