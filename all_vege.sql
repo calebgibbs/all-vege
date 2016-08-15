f1ec6834e84bdafc739ca28fddd61945ee81edff
Caleb Gibbs <calebgibbs@me.com>
2016-08-15 19:37:14 +1200

    Updating the database

--- all_vege.sql
+++ all_vege.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 06, 2016 at 12:52 AM
+-- Generation Time: Aug 15, 2016 at 07:16 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -19,6 +19,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `all_vege`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipes`
+--
+
+CREATE TABLE `recipes` (
+  `id` int(10) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `ingredients` text NOT NULL,
+  `method` text NOT NULL,
+  `serves` enum('1','2','3','4','5','6','7','8','9','10') NOT NULL,
+  `tags` set('side','breakfast','lunch','dinner','dessert','baked','beverage') NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -43,11 +62,19 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `profile_picture`, `password`, `privilege`) VALUES
 (1, 'Caleb', 'Gibbs', 'calebgibbs@me.com', '', '$2y$10$uN1fPRNEof5SdJ2rBzikouhMHa9bV7/dUyeyYMVFEq/whnQJc7fci', 'admin'),
 (2, 'User', 'Account', 'user@test.com', '', '$2y$10$/eMs0H3mvq6hXTUAzz/1Wu9AfRRJJZwbIKAq8LTPZE0U0QiLTpkoS', 'user'),
-(3, 'Moderator ', 'Account', 'mod@test.com', '', '$2y$10$lwO.Gm1152cLfCBVeFFUQ.fB71g7Fx970HMWJwgUJ.OwIjI9KdEe6', 'moderator');
+(3, 'Moderator ', 'Account', 'mod@test.com', '', '$2y$10$lwO.Gm1152cLfCBVeFFUQ.fB71g7Fx970HMWJwgUJ.OwIjI9KdEe6', 'moderator'),
+(4, 'New', 'Account', 'new@test.com', '', '$2y$10$0CLJaJrMTTz9p2J.O7RKvOJHFKNNtQ9p.QHDUG8Q5GzK2WqKpn5PK', 'user');
+
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `recipes`
+--
+ALTER TABLE `recipes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +88,15 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `recipes`
+--
+ALTER TABLE `recipes`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
